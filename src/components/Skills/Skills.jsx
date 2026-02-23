f3492641eda26a6489c726571a42eaf1fbd3950d
Oguguom Lawrence <lawrencecally3@gmail.com>
2026-02-23 16:49:01 +0300

    Revamped the skills section with appropriate titles and skill lists

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -15,27 +15,26 @@ const Skills = () => {
                     </div>
                 </div>
                 <div className="skill-container2">
-                    <h4 className="skill-title">📡Project & Business Strategy</h4>
+                    <h4 className="skill-title">📡 Telecom & Emerging Technologies</h4>
                     <div className="skill-list">
-                        <p>• Project Management</p>
-                        <p>• Planning & Implementation</p>
-                        <p>• Research & Business Development</p>
+                        <p>• 5G Expertise</p>
+                        <p>• Internet of Things (IoT)</p>
+                        <p>• B2B & B2C Strategy</p>
                     </div>
                 </div>
                 <div className="skill-container3">
-                    <h4 className="skill-title">🌐Project & Business Strategy</h4>
+                    <h4 className="skill-title">🌐 Networking & Systems</h4>
                     <div className="skill-list">
-                        <p>• Project Management</p>
-                        <p>• Planning & Implementation</p>
-                        <p>• Research & Business Development</p>
+                        <p>• Network Design & Implementation</p>
+                        <p>• Router & Switch Configuration (eNSP)</p>
+                        <p>• Troubleshooting</p>
                     </div>
                 </div>
                 <div className="skill-container4">
                     <h4 className="skill-title">⚡ Electrical Engineering</h4>
                     <div className="skill-list">
-                        <p>• Project Management</p>
-                        <p>• Planning & Implementation</p>
-                        <p>• Research & Business Development</p>
+                        <p>• Electrical Wiring & Installation</p>
+                        <p>• Substation Transformer Maintenance</p>
                     </div>
                 </div>
                 <div className="skill-container5">
