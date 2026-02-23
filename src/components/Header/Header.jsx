f5e95cea8ec326c318effba8684fff3eb7d7d859
Oguguom Lawrence <lawrencecally3@gmail.com>
2026-02-23 16:26:14 +0300

    Modified the nav-links as resquested

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#services">Services</a></li>
-                <li><a href="#projects">Projects</a></li>
+                <li><a href="#Skills">Skills</a></li>
                 <li><a href="#certificates">Certifications</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
