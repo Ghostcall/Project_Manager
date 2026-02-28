7388f84b67a3cd421fb12f4fc459ad8251cb636d
Oguguom Lawrence <lawrencecally3@gmail.com>
2026-02-28 02:23:51 +0200

    Revamped the certificate section for huawei certified special associate, digital power

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Certificate.css'
 import hcia from '../../assets/HCIA-Datacom(pic).jpg'
-import { BiColor } from 'react-icons/bi'
+import hcip from '../../assets/HCIP-Datacom.jpg'
+import digital from '../../assets/Screenshot 2026-02-28 020013.png'
+
 const Certificate = () => {
   return (
     <div className='certificate'>
@@ -12,7 +14,7 @@ const Certificate = () => {
                 <div className="certificate-up-description">
                     <img src= {hcia} alt="" />
                     <h3>
-                        <span>HCIA</span> Huawei Certified ICT Associate
+                        <span>HCIA</span> Datacom
                     </h3>
                 </div>
                 
@@ -21,55 +23,40 @@ const Certificate = () => {
                         IP addressing, routing & switching, WLAN, and basic network security.
                     </p>
 
-                    <a href="/src/assets/HCIA Datacom.pdf">View</a>
-
-                </div>
-            </div>
-            <div className="certificate-container">
-                <div className="certificate-up-description">
-                    <img src= {hcia} alt="" />
-                    <h3>
-                        <span>HCIP</span> Huawei Certified ICT Associate
-                    </h3>
-                </div>
-                
-                <div className="certificate-down-description">
-                    <p>Completed Huawei HCIA-Datacom Certification covering networking fundamentals,
-                        IP addressing, routing & switching, WLAN, and basic network security.
-                    </p>
-                    <a href="/src/assets/HCIA Datacom.pdf">View</a>
+                    <a href="/src/assets/HCIA Datacom.pdf">View Certificate</a>
 
                 </div>
             </div>
             <div className="certificate-container">
                 <div className="certificate-up-description">
-                    <img src= {hcia} alt="" />
+                    <img src= {hcip} alt="" />
                     <h3>
-                        <span>HCIP</span> Huawei Certified ICT Professional
+                        <span>HCIP</span> Datacom
                     </h3>
                 </div>
                 
                 <div className="certificate-down-description">
-                    <p>Completed Huawei HCIA-Datacom Certification covering networking fundamentals,
-                        IP addressing, routing & switching, WLAN, and basic network security.
+                    <p>
+                        Completed Huawei HCIP-Datacom Certification covering advanced routing protocols,
+                         enterprise switching, network optimization, troubleshooting, and secure network design
                     </p>
-                    <a href="/src/assets/HCIA Datacom.pdf">View</a>
+                    <a href="/src/assets/HCIP Certificate.pdf">View Certificate</a>
 
                 </div>
             </div>
             <div className="certificate-container">
                 <div className="certificate-up-description">
-                    <img src= {hcia} alt="" />
+                    <img src= {digital} alt="" />
                     <h3>
-                        HCIA - Huawei Certified ICT Associate
+                        <span>HCSA</span> Digital Power
                     </h3>
                 </div>
                 
                 <div className="certificate-down-description">
-                    <p>Completed Huawei HCIA-Datacom Certification covering networking fundamentals,
-                        IP addressing, routing & switching, WLAN, and basic network security.
+                    <p>Completed Huawei HCSA-Digital Power Certification covering data center power systems, 
+                        UPS technologies, energy storage, photovoltaic (solar) systems and intelligent power management solutions.
                     </p>
-                    <a href="/src/assets/HCIA Datacom.pdf">View</a>
+                    <a href="/src/assets/HCSA Digital Power.pdf">View Certificate</a>
 
                 </div>
             </div>
@@ -85,7 +72,23 @@ const Certificate = () => {
                     <p>Completed Huawei HCIA-Datacom Certification covering networking fundamentals,
                         IP addressing, routing & switching, WLAN, and basic network security.
                     </p>
-                    <a href="/src/assets/HCIA Datacom.pdf">View</a>
+                    <a href="/src/assets/HCIA Datacom.pdf">View Certificate</a>
+
+                </div>
+            </div>
+            <div className="certificate-container">
+                <div className="certificate-up-description">
+                    <img src= {hcia} alt="" />
+                    <h3>
+                        HCIA - Huawei Certified ICT Associate
+                    </h3>
+                </div>
+                
+                <div className="certificate-down-description">
+                    <p>Completed Huawei HCIA-Datacom Certification covering networking fundamentals,
+                        IP addressing, routing & switching, WLAN, and basic network security.
+                    </p>
+                    <a href="/src/assets/HCIA Datacom.pdf">View Certificate</a>
 
                 </div>
             </div>
